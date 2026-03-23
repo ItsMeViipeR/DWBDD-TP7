3568e7f24d7af4ee971e15c66807269ce8f3fc34
ItsMeViipeR <itsmeviiper@gmail.com>
2026-03-23 12:16:33 +0100

    feat: Add construct_response

---
+++ cgi-bin/construct_response.py
@@ -0,0 +1,8 @@
+def construct_response(code: int, message: str) -> str:
+    response = f"HTTP/1.1 {code} OK\r\n"
+    response += "Content-Type: text/html; charset=utf-8\r\n"
+    response += f"Content-Length: {len(message.encode("utf-8"))}"
+    response += "\r\n"
+    response += message
+
+    return response.encode("utf-8")
